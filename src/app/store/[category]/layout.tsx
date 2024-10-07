a840d3ed26b57c2e1dec5f1549bf2b343916bf59
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-07 17:31:18 -0600

    05: layouts

---
+++ src/app/store/[category]/layout.tsx
@@ -0,0 +1,10 @@
+export default function CategoryLayout(
+    { children }: { children: React.ReactNode }
+) {
+    return (
+        <main>
+            <nav>Navegacion de las categorias</nav>
+            {children}
+        </main>
+    )
+}
