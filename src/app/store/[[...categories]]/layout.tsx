9f7244c3d0f109f719451b568a30294bf5a7700b
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-08 17:39:52 -0600

    30: data fetching en nextjs

--- src/app/store/[[...categories]]/layout.tsx
+++ src/app/store/[[...categories]]/layout.tsx
@@ -1,9 +1,22 @@
-export default function CategoryLayout(
+import { getCollections } from "app/services/shopify/collections";
+import Link from "next/link";
+
+export default async function CategoryLayout(
     { children }: { children: React.ReactNode }
 ) {
+
+    const collections = await getCollections();
+    console.log(collections);
+
     return (
         <main>
-            <nav>Navegacion de las categorias</nav>
+            <nav>
+                {
+                    collections.map((c: any) => (
+                        <Link key={c.id} href={'store/' + c.handle}> {c.title} </Link>
+                    ))
+                }
+            </nav>
             {children}
         </main>
     )
---
+++ src/services/shopify/collections.ts
@@ -0,0 +1,26 @@
+import { env } from "app/config/env";
+import { shopifyUrls } from "./urls";
+
+export const getCollections = async () => {
+    try {
+        const response = await fetch(shopifyUrls.collections.all, {
+          headers: new Headers({
+            'X-Shopify-Access-Token': env.SHOPIFY_TOKEN ?? ''
+          })
+        })
+        const { smart_collections } = await response.json()
+
+        const transformedCollections = smart_collections.map((c: any) => {
+            return {
+                id: c.id,
+                title: c.title,
+                handle: c.handle
+            };
+        });
+
+        return transformedCollections;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
