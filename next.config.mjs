dbbfd099638112dbf0680823c0871121845c918c
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-08 13:38:33 -0600

    21: manejos de estados de carga loading

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-
 const nextConfig = {
     sassOptions: {
         prependData: `@import "src/sass/main.sass"`
     },
+    images: {
+        remotePatterns: [
+            {
+                hostname: 'cdn.shopify.com',
+                protocol: 'https'
+            }
+        ]
+    }
 };
 
 export default nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Roboto } from 'next/font/google'
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
 import 'app/sass/global.sass';
+import { Hero } from "app/components/home/Hero";
+import { Description } from "app/components/home/Description";
 
 const roboto = Roboto({
   weight: ["100", "300", "400", "500", "700"],
@@ -23,9 +25,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={roboto.className}>
         <Header />
-
+        <Hero />
+        <Description />
         {children}
-
         <Footer />
       </body>
     </html>
