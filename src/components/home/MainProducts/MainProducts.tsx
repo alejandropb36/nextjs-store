d2058ff306b3d4d3acfd7845062e96afa4474661
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-08 16:52:57 -0600

    27: arquitectura de servicios en nextjs

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,29 +1,17 @@
-import Image from 'next/image'
-import styles from './MainProducts.module.sass'
+import Image from 'next/image';
+import styles from './MainProducts.module.sass';
+import { getProducts } from 'app/services/shopify';
 
-const getProducts = async () => {
-    try {
-        const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2023-10/products.json`, {
-          headers: new Headers({
-            'X-Shopify-Access-Token': process.env.SHOPIFY_API_KEY || ""
-          })
-        })
-        const { products } = await response.json()
-        return products;
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
+
 
 export const MainProducts = async () => {
-  const products = await getProducts()
-  console.log(products)
+
+  const products = await getProducts();
   return (
     <section className={styles.MainProducts}>
       <h3>✨ New products released!</h3>
       <div className={styles.MainProducts__grid}>
-        {products?.map((product) => {
+        {products?.map((product: any) => {
           const imageSrc = product.images[0].src;
           return (
             <article key={product.id}>
