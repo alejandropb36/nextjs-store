f061ba1b36dd056f9985c7a77d19b0695df709c1
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-08 10:41:04 -0600

    12: estilos globales

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import { Header } from "app/components/shared/Header";
 import { Footer } from "app/components/shared/Footer";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import 'app/sass/global.sass';
 
 export const metadata: Metadata = {
   title: "Future world",
