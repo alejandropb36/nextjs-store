9ff9e9f45a06db36288854995cd220cd41cceb51
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-08 09:58:27 -0600

    10: css modules

--- src/components/home/Hero/Hero.tsx
+++ src/components/home/Hero/Hero.tsx
@@ -1,5 +1,11 @@
+import styles  from './Hero.module.css';
+
 export const Hero = () => {
     return (
-        <section>Hero</section>
+        <section className={styles.Hero}>
+            <h1>Future world</h1>
+
+            <h2>Empowering Your Tomorrow, Today!</h2>
+        </section>
     )
 }
