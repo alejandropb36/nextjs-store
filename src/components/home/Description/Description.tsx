a9d1d98239840234048c9cc18ee0d8c8c365524c
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-08 10:56:24 -0600

    13: archivos estaticos

--- src/components/home/Description/Description.tsx
+++ src/components/home/Description/Description.tsx
@@ -1,7 +1,13 @@
+import styles from './Description.module.sass';
+
 export const Description = () => {
     return(
-        <section>
-            <h1>Descripcion</h1>
+        <section className={styles.Description}>
+            <img src="/images/description.jpeg" alt="products marketplace" />
+            <div className={styles.Description__text}>
+                <h2>Description</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos qui cumque in perferendis amet? Reprehenderit dignissimos ut laboriosam sint ex distinctio molestiae pariatur provident quod voluptatum. Sunt provident nulla doloremque!</p>
+            </div>
         </section>
     )
 }
