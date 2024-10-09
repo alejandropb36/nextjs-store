a637de5489ad8d16ec728e79ed81f197c85bd404
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-09 13:30:05 -0600

    51: implementacion del chatbot

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,12 +1,20 @@
 import { Chat } from "app/components/chat/chat"
+import { getProducts } from "app/services/shopify/products"
+import { createAgent } from "app/utils/openai/createAgent";
 
 type Props = {}
 
-export default function ChatPage({}: Props) {
+export default async function ChatPage({}: Props) {
+  const products = await getProducts();
+  const productTitles = products.map(p => p.title);
+  const flapProductTitles = productTitles.join('\n');
+
+  const agent = createAgent(flapProductTitles);
+
   return (
     <>
         <h1>Chat</h1>
-        <Chat />
+        <Chat agent={agent}/>
     </>
   )
 }
