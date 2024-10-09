3b59964e54a60de1d9ba7f52239e7a40f517f324
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-09 13:15:24 -0600

    50: crear componente de chatbot

---
+++ src/app/chat/page.tsx
@@ -0,0 +1,12 @@
+import { Chat } from "app/components/chat/chat"
+
+type Props = {}
+
+export default function ChatPage({}: Props) {
+  return (
+    <>
+        <h1>Chat</h1>
+        <Chat />
+    </>
+  )
+}
