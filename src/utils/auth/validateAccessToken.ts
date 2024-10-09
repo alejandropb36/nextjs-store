c8049d2e5ee7474b283d0cff41f74bb8c51bd989
Alejandro Ponce <luis.ponce@contpaqi.com>
2024-10-09 11:26:40 -0600

    49: middlewares en nextjs

--- src/utils/auth/validateAccessToken.ts
+++ src/utils/auth/validateAccessToken.ts
@@ -1,15 +1,17 @@
-import { GraphQLClientSingleton } from "app/graphql";
-import { customerName } from "app/graphql/queries/customerName";
-import { cookies } from "next/headers"
+import { GraphQLClientSingleton } from 'app/graphql'
+import { customerName } from 'app/graphql/queries/customerName'
+import { cookies } from 'next/headers'
 
 export const validateAccessToken = async () => {
-    const cookiesStore = cookies();
-    const accessToken = cookiesStore.get('accessToken')?.value;
-
+  try {
+    const cookieStore = cookies()
+    const accessToken = cookieStore.get('accessToken')?.value || ''
     const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
-    const { customer }: any = await graphqlClient.request(customerName, {
-        customerAccessToken: accessToken
-    });
-
-    return customer;
+    const { customer } = await graphqlClient.request(customerName, {
+      customerAccessToken: accessToken
+    })
+    return customer
+  } catch (error) {
+    console.error(error)
+  }
 }
